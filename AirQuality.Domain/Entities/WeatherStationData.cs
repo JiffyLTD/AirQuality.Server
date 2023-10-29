02e72364df0f1aaa512a9862db6f51a02fdc8b6b
Jiffy <120106650+JiffyLTD@users.noreply.github.com>
2023-10-29 18:15:33 +0300

    добавлены новые датчики

--- AirQuality.Domain/Entities/WeatherStationData.cs
+++ AirQuality.Domain/Entities/WeatherStationData.cs
@@ -2,17 +2,25 @@
 {
     public class WeatherStationData
     {
-        public WeatherStationData(float temperature, float humidity)
+        public WeatherStationData(float temperature, float humidity, float pm_1, float pm2_5, float pm_10, int co)
         {
             ID = new Guid();
             Temperature = temperature;
             Humidity = humidity;
             CreationDate = DateTime.Now;
+            Pm_1 = pm_1;
+            Pm2_5 = pm2_5;
+            Pm_10 = pm_10;
+            Co = co;
         }
 
         public Guid ID { get; set; }
         public float Temperature { get; set; }
         public float Humidity { get; set; }
+        public float Pm_1 { get; set; }
+        public float Pm2_5 { get; set; }
+        public float Pm_10 { get; set; }
+        public int Co { get; set; }
         public DateTime CreationDate { get; set; }
     }
 }
